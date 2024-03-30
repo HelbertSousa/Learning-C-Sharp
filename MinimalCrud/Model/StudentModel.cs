e577fc0ac5b88569fb26e98c5befc29513284062
Helbert <helbertsousar@gmail.com>
2024-03-29 22:21:37 -0300

    feat: add student model

---
+++ MinimalCrud/Model/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MinimalCrud.Model
+{
+    public class StudentModel
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+
+        public string Name { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+    }
+}
