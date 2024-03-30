cab29f3e6b3b9f561d4a0cb83f6965a1850462ea
Helbert <helbertsousar@gmail.com>
2024-03-30 20:15:18 -0300

    feat: add new routes from students

--- MinimalCrud/Program.cs
+++ MinimalCrud/Program.cs
@@ -25,5 +25,54 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
+app.MapGet("/Hello Cruel World!!", () => "Hello Cruel World");
+
+app.MapGet("/Student", async (ApDbContext dbContext) =>
+{
+    return await dbContext.Students.ToListAsync();
+});
+
+app.MapGet("/Student/{id}", async (ApDbContext dbContext, Guid id) =>
+{
+    var student = await dbContext.Students.FindAsync(id);
+    if (student == null)
+    {
+        return Results.NotFound("The student couldn't be found.");
+    }
+    return Results.Ok(student);
+});
+
+app.MapPost("/Student", async (ApDbContext dbContext, MinimalCrud.Model.StudentModel student) =>
+{
+    dbContext.Students.Add(student);
+    await dbContext.SaveChangesAsync();
+    return Results.Created($"/students/{student.Id}", student);
+});
+
+app.MapPut("/Student", async (ApDbContext dbContext, MinimalCrud.Model.StudentModel student) =>
+{
+    var editStudent = await dbContext.Students.AsNoTracking().FirstOrDefaultAsync(editStudent => editStudent.Id == student.Id);
+    
+    if (editStudent is null) return Results.BadRequest("You can't edit a missing student");
+
+    dbContext.Entry(editStudent).CurrentValues.SetValues(student);
+
+    dbContext.Students.Update(editStudent);
+    
+    await dbContext.SaveChangesAsync();
+    return Results.Ok("Student Update");
+});
+
+app.MapDelete("/Student/{id}", async (ApDbContext dbContext, Guid id) =>
+{
+    var student = await dbContext.Students.FindAsync(id);
+    if (student == null)
+    {
+        return Results.NotFound("The student couldn't be found.");
+    }
+    dbContext.Students.Remove(student);
+    await dbContext.SaveChangesAsync();
+    return Results.Ok("Student deleted");
+});
 
 app.Run();
