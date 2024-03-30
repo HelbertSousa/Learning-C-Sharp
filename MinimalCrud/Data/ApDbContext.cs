5b72171e89572f2f364b8ed4c4ea3215daa5fc68
Helbert <helbertsousar@gmail.com>
2024-03-29 22:21:47 -0300

    feat: add config db sql server

---
+++ MinimalCrud/Data/ApDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalCrud.Data
+{
+    public class ApDbContext(DbContextOptions<ApDbContext> options) : DbContext(options)
+    {
+        public DbSet<Model.StudentModel> Students { get; set; }
+    }
+}
