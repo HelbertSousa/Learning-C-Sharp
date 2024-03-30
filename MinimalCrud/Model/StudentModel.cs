463397790ee17c5b44f1b5f77208c636ee102d5b
Helbert <helbertsousar@gmail.com>
2024-03-29 22:45:49 -0300

    feat: add data annotations to student

--- MinimalCrud/Model/StudentModel.cs
+++ MinimalCrud/Model/StudentModel.cs
@@ -1,16 +1,34 @@
-﻿namespace MinimalCrud.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MinimalCrud.Model
 {
     public class StudentModel
     {
+        [Key]
         public Guid Id { get; init; } = Guid.NewGuid();
 
+        [Required(ErrorMessage = "Informe seu nome")]
+        [StringLength(80, ErrorMessage = "Nome não pode possuir mais que 80 caracteres")]
+        [MinLength(5, ErrorMessage = "Nome deve possuir no mínimo 5 caracteres")]
+        [DisplayName("Nome")]
         public string Name { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Informe seu Telefone.")]
+        [Phone]
+        [DisplayName("Telefone")]
         public string PhoneNumber { get; set; } = string.Empty;
 
+        [StringLength(100, ErrorMessage = "Endereço não pode possuir mais que 100 caracteres")]
+        [MinLength(20, ErrorMessage = "Endereço deve possuir no mínimo 20 caracteres")]
+        [DisplayName("Endereço")]
         public string Address { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Informe seu Email.")]
+        [EmailAddress]
+        [DisplayName("Email")]
         public string Email { get; set; } = string.Empty;
+
 
     }
 }
